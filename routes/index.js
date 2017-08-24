a15a7fe0ec617317466280a600be98b65121eda4
brunouno <bruno.navarreteon@gmail.com>
2017-08-24 10:26:27 -0500

    added initial GET routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// GET root
+	router.get('/',function(req,res){
+		res.send('../public/index.html');
+	});
+
+// GET register
+	router.get('/register',function(req,res){
+		res.sendFile(path.join(__dirname + '/../public/register.html'));
+	});
 
 module.exports = router;
